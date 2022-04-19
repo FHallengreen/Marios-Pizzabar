ad5aad71661104e4683b18e0129bb241991c225d
KaptajnKirk <theodor@duus-kirk.dk>
2022-04-19 22:51:51 +0200

    Alligned menu (temp)

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -6,20 +6,20 @@ public class Menu {
   ArrayList<Pizza> pizzaMenu = new ArrayList();
 
   public Menu() {
-    Pizza pizza1 = new Pizza(1, "Vesuvio", "Tomatsauce, ost, skinke & oregano.", 57);
-    Pizza pizza2 = new Pizza(2, "Amerikaner", "Tomatsauce, ost, oksefars & oregano.", 53);
-    Pizza pizza3 = new Pizza(3, "Cacciatore", "Tomatsauce, ost, pepperoni & oregano.", 57);
-    Pizza pizza4 = new Pizza(4,"Carbona","Tomatsauce, ost, kødsauce, spaghetti, cocktailpølser & oregano", 63);
-    Pizza pizza5 = new Pizza(5,"Dennis","Tomatsauce, ost, skinke, pepperoni, cocktailpølser & oregano.", 65);
-    Pizza pizza6 = new Pizza(6,"Bertil","Tomatsauce, ost, bacon & oregano.", 57);
-    Pizza pizza7 = new Pizza(7,"Silvia","Tomatsauce, ost, pepperoni, rød peber, løg, oliven & oregano.", 61);
-    Pizza pizza8 = new Pizza(8,"Victoria","Tomatsauce, ost, skinke, ananas, champignon, løg & oregano.", 61);
-    Pizza pizza9 = new Pizza(9,"Toronfo","Tomatsauce, ost, skinke, bacon, kebab, chili & oregano.", 61);
-    Pizza pizza10 = new Pizza(10,"Capricciosa","Tomatsauce, ost, skinke, champignon & oregano.", 61);
-    Pizza pizza11 = new Pizza(11,"Hawaii","Tomatsauce, ost, skinke, ananas & oregano.", 61);
-    Pizza pizza12 = new Pizza(12,"Le Blissola","Tomatsauce, ost, skinke, rejer & oregano.", 61);
-    Pizza pizza13 = new Pizza(13,"Venezia","Tomatsauce, ost, bacon & oregano.", 61);
-    Pizza pizza14 = new Pizza(14,"Mafia","Tomatsauce, ost, pepperoni, bacon, løg & oregano.", 61);
+    Pizza pizza1 = new Pizza(1,"Vesuvio", "\t Tomatsauce, ost, skinke & oregano.\t\t\t\t\t\t\t", 57);
+    Pizza pizza2 = new Pizza(2,"\t Amerikaner", "\t Tomatsauce, ost, oksefars & oregano.\t\t\t\t\t\t", 53);
+    Pizza pizza3 = new Pizza(3,"\t Cacciatore", "\t Tomatsauce, ost, pepperoni & oregano.\t\t\t\t\t\t", 57);
+    Pizza pizza4 = new Pizza(4,"\t Carbona","\t Tomatsauce, ost, kødsauce, spaghetti, cocktailpølser & oregano", 63);
+    Pizza pizza5 = new Pizza(5,"\t Dennis","\t\t Tomatsauce, ost, skinke, pepperoni, cocktailpølser & oregano.", 65);
+    Pizza pizza6 = new Pizza(6,"\t Bertil","\t\t Tomatsauce, ost, bacon & oregano.\t\t\t\t\t\t\t", 57);
+    Pizza pizza7 = new Pizza(7,"\t Silvia","\t\t Tomatsauce, ost, pepperoni, rød peber, løg, oliven & oregano.", 61);
+    Pizza pizza8 = new Pizza(8,"\t Victoria","\t Tomatsauce, ost, skinke, ananas, champignon, løg & oregano.", 61);
+    Pizza pizza9 = new Pizza(9,"\t Toronfo","\t Tomatsauce, ost, skinke, bacon, kebab, chili & oregano.\t", 61);
+    Pizza pizza10 = new Pizza(10,"Capricciosa","Tomatsauce, ost, skinke, champignon & oregano.\t\t\t\t", 61);
+    Pizza pizza11 = new Pizza(11,"Hawaii","\t\t Tomatsauce, ost, skinke, ananas & oregano.\t\t\t\t\t", 61);
+    Pizza pizza12 = new Pizza(12,"Le Blissola","Tomatsauce, ost, skinke, rejer & oregano.\t\t\t\t\t", 61);
+    Pizza pizza13 = new Pizza(13,"Venezia","\t Tomatsauce, ost, bacon & oregano.\t\t\t\t\t\t\t", 61);
+    Pizza pizza14 = new Pizza(14,"Mafia","\t\t Tomatsauce, ost, pepperoni, bacon, løg & oregano.\t\t\t", 61);
 
     addPizza(pizza1);
     addPizza(pizza2);
--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -23,7 +23,7 @@ public class Pizza {
 
   @Override
   public String toString() {
-    String text = "\u001B[1m " + number  + ". " +  name + "\t\t \033[0m" + ingredients + "\t\t\t\t" + price + ",-" + "\n ";
+    String text = "\u001B[1m " + number  + ".\t\t " +  name + "\t\t \033[0m" + ingredients + "\t\t\t\t" + price + ",-" + "\n ";
   return text;
   }
 
