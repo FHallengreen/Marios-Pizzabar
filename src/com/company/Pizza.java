40deb6066f393fbc9d7776cacaa05d77a4915638
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-04-19 14:41:09 +0200

    Added pizzaClass

---
+++ src/com/company/Pizza.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Pizza {
+    private int number;
+    private String name;
+    private String ingredients;
+    private int price;
+
+    public Pizza (int number, String name, String ingredients, int price){
+       this.number = number;
+        this.name = name;
+       this.ingredients = ingredients;
+       this.price = price;
+    }
+
+}
